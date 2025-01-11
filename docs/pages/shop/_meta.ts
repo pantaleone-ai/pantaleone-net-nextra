871fce9dca102bedd7e2c139ffbe0dbfb2b90744
pantaleone-ai <mdptrading@gmail.com>
2025-01-11 11:53:10 -0700

    tweak menu

--- docs/pages/shop/_meta.ts
+++ docs/pages/shop/_meta.ts
@@ -15,21 +15,21 @@ export default {
 //       title: 'Artwork & NFTs',
 //       href: '/art-nfts'
 //     },
-  '-- Shop Pantaleone @ Marketplaces': {
-    type: 'separator',
-    title: 'Shop Pantaleone @ Marketplaces'
-  },
-  'next.js-linkos': {
-    title: 'OpenSea ↗',
-    type: 'page',
-    href: 'https://opensea.com',
-    newWindow: true
-  },
-  'next.js-linkbase': {
-    title: 'Coinbase ↗',
-    type: 'page',
-    href: 'https://coinbase.com',
-    newWindow: true
-  }
+  // '-- Shop Pantaleone @ Marketplaces': {
+  //   type: 'separator',
+  //   title: 'Shop Pantaleone @ Marketplaces'
+  // },
+  // 'next.js-linkos': {
+  //   title: 'OpenSea ↗',
+  //   type: 'page',
+  //   href: 'https://opensea.com',
+  //   newWindow: true
+  // },
+  // 'next.js-linkbase': {
+  //   title: 'Coinbase ↗',
+  //   type: 'page',
+  //   href: 'https://coinbase.com',
+  //   newWindow: true
+  // }
 //  }
 }
